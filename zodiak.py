98ce14c1ba8acca58f4086c709fa5820032d7bf8
Andrey Sharapov <penguin69@mail.ru>
2023-05-07 22:49:50 +0500

    add zodiak

---
+++ zodiak.py
@@ -0,0 +1,26 @@
+month = int(input('Input month: '))
+day = int(input('Input day: '))
+if (month == 12 and (22 <= day <= 31)) or (month == 1 and (1 <= day <= 19)):
+  print("Козерог")
+if (month == 1 and (20 <= day <= 31)) or (month == 2 and (1 <= day <= 19)):
+  print("Водолей")
+if (month == 2 and (20 <= day <= 31)) or (month == 3 and (1 <= day <= 21)):
+  print("Рыбы")
+if (month == 3 and (22 <= day <= 31)) or (month == 4 and (1 <= day <= 21)):
+  print("Овен")
+if (month == 4 and (22 <= day <= 31)) or (month == 5 and (1 <= day <= 21)):
+  print("Телец")
+if (month == 5 and (22 <= day <= 31)) or (month == 6 and (1 <= day <= 21)):
+  print("Близнецы")
+if (month == 6 and (22 <= day <= 31)) or (month == 7 and (1 <= day <= 21)):
+  print("Рак")
+if (month == 7 and (22 <= day <= 31)) or (month == 8 and (1 <= day <= 21)):
+  print("Лев")
+if (month == 8 and (22 <= day <= 31)) or (month == 9 and (1 <= day <= 21)):
+  print("Дева")
+if (month == 9 and (22 <= day <= 31)) or (month == 10 and (1 <= day <= 21)):
+  print("Весы")
+if (month == 10 and (22 <= day <= 31)) or (month == 11 and (1 <= day <= 20)):
+  print("Скорпион")
+if (month == 11 and (21 <= day <= 31)) or (month == 12 and (1 <= day <= 21)):
+  print("Стрелец")
